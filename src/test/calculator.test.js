2f0e653d47ae85d59a870706d2fd347c8ea35c35
dokacoder <dokacoder@gmail.com>
2022-08-05 15:05:09 +0900

    add test clear/add/subtract/multiply/divides

--- src/test/calculator.test.js
+++ src/test/calculator.test.js
@@ -14,4 +14,47 @@ describe('Calculator', () => {
     cal.set(9);
     expect(cal.value).toBe(9);
   });
+
+  it('clear', () => {
+    cal.set(9);
+    cal.clear();
+    expect(cal.value).toBe(0);
+  });
+
+  it('add', () => {
+    cal.set(9);
+    cal.add(1);
+    expect(cal.value).toBe(10);
+  });
+
+  it('subtract', () => {
+    cal.set(9);
+    cal.subtract(1);
+    expect(cal.value).toBe(8);
+  });
+
+  it('multiply', () => {
+    cal.set(9);
+    cal.multiply(2);
+    expect(cal.value).toBe(18);
+  });
+
+  describe('divides', () => {
+    it('0 / 0 === NaN', () => {
+      cal.divide(0);
+      expect(cal.value).toBe(NaN);
+    });
+
+    it('1 / 0 === Infinity', () => {
+      cal.set(1);
+      cal.divide(0);
+      expect(cal.value).toBe(Infinity);
+    });
+
+    it('4 / 4 === 1', () => {
+      cal.set(4);
+      cal.divide(4);
+      expect(cal.value).toBe(1);
+    });
+  });
 });
